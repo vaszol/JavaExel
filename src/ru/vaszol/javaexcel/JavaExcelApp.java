fc7663a39af141f96e68a8acfe1add3b831292b9
vaszol <vaszol@mail.ru>
2015-07-07 11:15:59 +0300

    создаём ячейки в книге на разных листах

--- src/ru/vaszol/javaexcel/JavaExcelApp.java
+++ src/ru/vaszol/javaexcel/JavaExcelApp.java
@@ -1,6 +1,8 @@
 package ru.vaszol.javaexcel;
 
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.util.WorkbookUtil;
@@ -13,7 +15,21 @@ public class JavaExcelApp {
 	public static void main(String[] args) throws IOException{
 		Workbook wb = new HSSFWorkbook(); //объект excel книги
 		Sheet sheet0=wb.createSheet("Издатели"); //создаёт страницу в книге
-		Sheet sheet1=wb.createSheet("Книги"); //создаёт страницу в книге
+
+        Row row0 = sheet0.createRow(3); //создали строку в листе
+        Cell cell0=row0.createCell(4); //создали ячейку в строке
+        cell0.setCellValue("O'Reilly"); //записываем данные в ячейку
+
+		Sheet sheet1=wb.createSheet("Произведения"); //создаёт страницу в книге
+
+        Row row1 = sheet1.createRow(0); //создали строку в листе
+        Cell cell1 = row1.createCell(0); //создали ячейку в строке
+        cell1.setCellValue("Война и мир"); //записываем данные в ячейку
+
+        Row row2 = sheet1.createRow(1); //создали строку в листе
+        Cell cell2 = row2.createCell(3); //создали ячейку в строке
+        cell2.setCellValue("Евгений онегин"); //записываем данные в ячейку
+
 		Sheet sheet2=wb.createSheet("Авторы"); //создаёт страницу в книге
 		Sheet sheet3=wb.createSheet(WorkbookUtil.createSafeSheetName("валоыврало?*Г:?**№?*?*")); //создаёт страницу в книге с нестандартным именем
 
