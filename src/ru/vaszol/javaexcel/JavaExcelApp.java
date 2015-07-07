0f60ee11f4c6d6ee2ff66a02a4a23b53c72dc16c
vaszol <vaszol@mail.ru>
2015-07-07 10:59:10 +0300

    работаем с листами книги

--- src/ru/vaszol/javaexcel/JavaExcelApp.java
+++ src/ru/vaszol/javaexcel/JavaExcelApp.java
@@ -3,6 +3,7 @@ package ru.vaszol.javaexcel;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.util.WorkbookUtil;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -11,8 +12,11 @@ public class JavaExcelApp {
 
 	public static void main(String[] args) throws IOException{
 		Workbook wb = new HSSFWorkbook(); //объект excel книги
-		Sheet sheet=wb.createSheet("MySheet"); //создаёт страницу в книге
-		
+		Sheet sheet0=wb.createSheet("Издатели"); //создаёт страницу в книге
+		Sheet sheet1=wb.createSheet("Книги"); //создаёт страницу в книге
+		Sheet sheet2=wb.createSheet("Авторы"); //создаёт страницу в книге
+		Sheet sheet3=wb.createSheet(WorkbookUtil.createSafeSheetName("валоыврало?*Г:?**№?*?*")); //создаёт страницу в книге с нестандартным именем
+
 		FileOutputStream fos = new FileOutputStream("my.xls"); //
 		
 		wb.write(fos);
